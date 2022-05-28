844c75d9478c4e3ea6bd772f78d6431bf8e307c2
Clement CHARLES <clement.charlesC@gmail.com>
2022-05-28 18:46:30 +0200

    Few compiler typeScript option

---
+++ public/test.js
@@ -0,0 +1,10 @@
+"use strict";
+// Si on veut avoir nos fichiers ts dans src et nos fichier compilé en JS dans public => tsconfig.js
+//! Par défaut, quand on compile, ça compile dans le même fichier que dans le ficher ts
+/*** rootDir à définir - outDir à définir  ***/ //! Line 27 & 28 tsconfig
+//? Include - Exclude - Line 102 tsconfig.json
+//compilerOption
+// tsconfig - lib
+//! If active, the default library wont work anymore
+//? https://www.typescriptlang.org/tsconfig#lib
+//TsConfig Option de base: 
